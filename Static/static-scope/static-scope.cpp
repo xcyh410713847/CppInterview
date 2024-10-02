6a507225151726030d4918f77b2f76951735eab7
Shyfan <410713847@qq.com>
2024-10-02 10:48:18 +0800

    static-scope

---
+++ Static/static-scope/static-scope.cpp
@@ -0,0 +1,19 @@
+﻿/*************************************************************************
+**文件: Static\static-scope\static-scope.cpp
+**作者: shyfan
+**日期: 2024/10/02 10:36:44
+**功能: 作用域
+*************************************************************************/
+
+#include <iostream>
+
+#include "static-scope1.h"
+#include "static-scope2.h"
+
+int main()
+{
+    std::cout << "g_static_scope1 = " << g_static_scope1 << std::endl;
+    std::cout << "g_static_scope2 = " << g_static_scope2 << std::endl;
+
+    return 0;
+}
