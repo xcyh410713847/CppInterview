496d6f0e44a736e3846d0ff2a1f6a30c80514469
Shyfan <410713847@qq.com>
2024-10-17 15:44:50 +0800

    class-inheritance

---
+++ Class/class-inheritance/class-inheritance.cpp
@@ -0,0 +1,31 @@
+﻿/*************************************************************************
+**文件: Class\class-inheritance\class-inheritance.cpp
+**作者: shyfan
+**日期: 2024/10/16 18:19:18
+**功能: 类的继承
+*************************************************************************/
+
+#include <iostream>
+
+class Base
+{
+private:
+    int m_n1;
+
+public:
+    Base(int n1) : m_n1(n1)
+    {
+        std::cout << "Base::Base()" << std::endl;
+    }
+};
+
+class Derived1 : public Base
+{
+};
+
+int main()
+{
+    // Derived1 d1(10); // 错误，没有合适的构造函数
+
+    return 0;
+}
